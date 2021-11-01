4fbf8c14705e5deca71dd63fc3784c1509625c36
Paul Court <paul.court@ctrlhub.com>
2021-11-01 15:02:53 +0000

    Adds github action build configurations.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,7 @@ module uk.co.flxs.rftool {
     requires javafx.controls;
     requires com.opencsv;
     
+    opens uk.co.flxs.rftool to javafx.graphics;
+    
     exports uk.co.flxs.rftool;
 }
---
+++ src/main/java/uk/co/flxs/rftool/AppLauncher.java
@@ -0,0 +1,7 @@
+package uk.co.flxs.rftool;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
